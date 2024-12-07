286013ced3453e50e5fb0fe36f0301f68a76335f
Nordin <128930417+nordin05@users.noreply.github.com>
2024-12-07 17:42:44 +0100

    finish styling

--- messages.js
+++ messages.js
@@ -8,6 +8,16 @@ const messages = [
         text: "Hello World!",
         user: "Charles",
         added: new Date()
+    },
+    {
+        text: "This is a small message board",
+        user: "Tim",
+        added: new Date()
+    },
+    {
+        text: "Press the button to add your own message",
+        user: "Jessie",
+        added: new Date()
     }
 ];
 
